9b60e272208b7d53362099e16b94adfefa65b923
Mohammad Zafar <mzcoder7@gmail.com>
2023-05-14 11:53:18 +0100

    get all users, get user by id

--- app.js
+++ app.js
@@ -1,8 +1,42 @@
 import express from "express";
 
+import {
+  getUsers,
+  getUserByID,
+  createUser,
+  updateUserByID,
+  deleteUserByID,
+} from "./users.js";
+
 const app = express();
 const port = 3000;
 
+// get all users
+app.get("/api/users", async (req, res) => {
+  const users = await getUsers();
+  let success = false;
+  if (users.length > 0) {
+    success = true;
+    console.log(`getUsers: successful`);
+  }
+  res.send({ success, payload: users });
+});
+
+// get user by id
+app.get("/api/users/:id", async (req, res) => {
+  const user = await getUserByID(req.params.id);
+  let success = false;
+  if (Object.keys(user).length > 0) {
+    success = true;
+    console.log(`getUserById: successful`);
+  }
+  res.send({ success, payload: user });
+});
+
+// create new user
+// app.post('/api/users',async(req,res)=>{
+//   const user=await
+// })
 app.use(express.json());
 
 app.use("/", (req, res) => {
--- users.js
+++ users.js
@@ -3,9 +3,22 @@ import { v4 as uuidv4 } from "uuid";
 
 const fileName = "users.json";
 
-export async function getUsers() {}
+export async function getUsers() {
+  // should return an array of all users
+  const usersJSON = await fs.readFile(fileName);
+  const users = JSON.parse(usersJSON);
+  return users;
+}
 
-export async function getUserByID(id) {}
+export async function getUserByID(id) {
+  // should return the particular user we are looking for
+  const usersJSON = await fs.readFile(fileName);
+  const users = JSON.parse(usersJSON);
+  const userFound = users.filter((user) => {
+    return user.id === id;
+  });
+  return userFound;
+}
 
 export async function createUser(newUser) {}
 
